44084291f4d331226c6931a28159522e20e10abf
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-07-16 01:54:48 +0300

    feat(01-starting-project): add header component

--- projects/01-starting-project/src/app/app.component.ts
+++ projects/01-starting-project/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
