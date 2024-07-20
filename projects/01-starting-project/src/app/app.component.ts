07e03f5d97fc35dc070bc23c690acd2274e133ca
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-07-21 02:17:32 +0300

    feat(01-starting-project): add user component

--- projects/01-starting-project/src/app/app.component.ts
+++ projects/01-starting-project/src/app/app.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS;
+
+  onSelectUser(id: string) {
+    console.log('Selected user with id: ' + id);
+  }
+}
