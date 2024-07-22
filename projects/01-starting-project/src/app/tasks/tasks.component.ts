e62b8a1ab38322813279b2ae04c30e404b51cf5e
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-07-22 23:51:59 +0300

    feat(01-starting-project): add task component

--- projects/01-starting-project/src/app/tasks/tasks.component.ts
+++ projects/01-starting-project/src/app/tasks/tasks.component.ts
@@ -1,11 +1,20 @@
 import { Component, Input } from '@angular/core';
+import { TaskComponent } from './task/task.component';
+import { dummyTasks } from '../dummy-tasks';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
+  imports: [TaskComponent],
 })
 export class TasksComponent {
+  @Input() userId?: string;
   @Input() name?: string;
+  tasks = dummyTasks;
+
+  get selectedUserTasks() {
+    return this.tasks.filter((task) => task.userId === this.userId);
+  }
 }
