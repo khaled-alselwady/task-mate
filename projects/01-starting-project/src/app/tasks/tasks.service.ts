8b3917102dcf938b917359b915638fa5911adbc3
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-07-26 01:12:36 +0300

    feat(01-starting-project/tasks): integrate LocalStorage for persistent task storage
    - Update `TasksService` to use LocalStorage for task persistence.
      - Load tasks from LocalStorage on service initialization.
      - Save tasks to LocalStorage whenever a task is added or removed.
    - Ensure that tasks persist across page reloads and browser sessions.
    
    Changes include:
    - **tasks.service.ts**:
      - Load tasks from LocalStorage if available during service construction.
      - Update `addTask` and `removeTask` methods to save changes to LocalStorage.
      - Add `saveToLocalStorage` method to handle saving tasks to LocalStorage.
    
    This commit enhances the task management functionality by persisting tasks using LocalStorage, ensuring data remains consistent across sessions.

--- projects/01-starting-project/src/app/tasks/tasks.service.ts
+++ projects/01-starting-project/src/app/tasks/tasks.service.ts
@@ -6,6 +6,14 @@ import { type NewTaskData } from './task/task.model';
 export class TasksService {
   private tasks = dummyTasks;
 
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -18,9 +26,16 @@ export class TasksService {
       summary: taskData.summary,
       dueDate: taskData.date,
     });
+
+    this.saveToLocalStorage();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+    this.saveToLocalStorage();
+  }
+
+  private saveToLocalStorage() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
