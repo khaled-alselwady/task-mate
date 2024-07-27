acd9fa2b93d93ee7dfd1e5d99188ddefd9299f56
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-07-27 22:19:04 +0300

    refactor(01-starting-project/shared): create SharedModule for shared components
    - Create `SharedModule` in the shared folder.
    - Move `CardComponent` to `SharedModule`.
    - Update `AppModule` to import `SharedModule`.
    
    Changes include:
    - **shared.module.ts**:
      - Create `SharedModule` and declare `CardComponent`.
      - Export `CardComponent` from `SharedModule`.
    - **app.module.ts**:
      - Remove `CardComponent` from the declarations array.
      - Import `SharedModule` to include shared components.
    
    This commit restructures the application to use a shared module for shared components, aligning with Angular's best practices.

--- projects/01-starting-project/src/app/app.module.ts
+++ projects/01-starting-project/src/app/app.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
-import { CardComponent } from './shared/card/card.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { TaskComponent } from './tasks/task/task.component';
 import { NewTaskComponent } from './tasks/new-task/new-task.component';
@@ -15,12 +15,11 @@ import { NewTaskComponent } from './tasks/new-task/new-task.component';
     AppComponent,
     HeaderComponent,
     UserComponent,
-    CardComponent,
     TasksComponent,
     TaskComponent,
     NewTaskComponent,
   ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, SharedModule],
 })
 export class AppModule {}
---
+++ projects/01-starting-project/src/app/shared/shared.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from '@angular/core';
+import { CardComponent } from './card/card.component';
+
+@NgModule({
+  declarations: [CardComponent],
+  exports: [CardComponent],
+})
+export class SharedModule {}
