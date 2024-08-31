8738c73b242b1b6d2d051236638bde86885ac37f
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-31 22:17:01 +0300

    feat: use routerLink directive

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,12 +1,14 @@
 import { Component, computed, input } from '@angular/core';
 
 import { type User } from './user.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   standalone: true,
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
+  imports: [RouterLink],
 })
 export class UserComponent {
   user = input.required<User>();
