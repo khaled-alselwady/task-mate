6b827c75d01f12ec1a2fca1b4cb05197050fbd07
dev-khaled-yousef <Khalidsliman2014@gmail.com>
2024-08-31 22:04:50 +0300

    feat: add a route for NO tasks component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,12 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
+import { NoTaskComponent } from './tasks/no-task/no-task.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: NoTaskComponent,
+  },
   {
     path: 'tasks',
     component: TasksComponent,
